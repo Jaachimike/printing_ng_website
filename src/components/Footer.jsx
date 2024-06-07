65f2c9c65bb3fcc400de7497d6298d43d40c4c3f
Omo <omoikheloa56@gmail.com>
2024-06-07 12:31:31 +0100

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,10 @@ import Logo from "../assets/svg/Printin logo.svg";
 import facebook from "../assets/svg/facebook_logo_icon.svg";
 import instagram from "../assets/svg/instagram_logo_icon.svg";
 import linkedin from "../assets/svg/linkedin_logo.svg";
+import mastercard from '../assets/svg/mastercard-svgrepo-com.svg';
+import visa from '../assets/svg/visa-logo-svgrepo-com.svg';
+import verve from '../assets/svg/verve-svgrepo-com.svg';
+import bank from '../assets/svg/bank-svgrepo-com.svg'
 import { Link } from "react-router-dom";
 
 const Footer = () => {
@@ -17,85 +21,79 @@ const Footer = () => {
     <footer className="bg-[#DFEFFF] py-8 pl-8 lg:pl-16 flex flex-col items-start md:flex-row md:justify-between md:items-start *:mb-6">
       {/* 1st column  */}
       <div className="sm:w-1/4">
-        <a href="/" className="flex items-center mb-4">
+        <a href="/" className="flex items-center -mt-2 mb-2">
           <img src={Logo} alt="Logo" className="w-auto h-16 mr-4" />
         </a>
-        <p className="text-gray-500 text-sm">
-          #59, AKEJU STREET,
-          <br />
-          OFF IKORODU ROAD, SHOMOLU,. LAGOS.
-          <br />
-          <a href="tel:+2348163534401">
-            <span className=" font-bold">Tel:</span> +234 816 353 4401
+        <a href="/" className="flex items-center -mt-2 mb-4 text-gray-500 ml-8 text-xs">About Us
           </a>
-          <br />
-          <a href="mailto:info@example.com">
-            Email: info@printin.ng | printingintelligence@gmail.com
-          </a>
-        </p>
-        <p className="text-gray-400 text-xs mt-2">
-          Copyright © 2024 All Rights Reserved
-        </p>
+        <a href="/" className="flex items-center -mt-2 mb-4 text-gray-500 ml-8 text-xs">
+          Contact</a>
+        <a href="/" className="flex items-center -mt-2 mb-4 text-gray-500 ml-8 text-xs">
+          Track Order</a>
+        <a href="/" className="flex items-center -mt-2 mb-4 text-gray-500 ml-8 text-xs">
+          Printin Promise</a>
+        <a href="/" className="flex items-center -mt-2 mb-4 text-gray-500 ml-8 text-xs">
+          Career</a>
       </div>
       {/* second column  */}
-      <div className="sm:w-1/4">
-        <h3 className=" text-lg font-medium mb-4">PRINTINGNG</h3>
+      <div className="mt-4 sm:w-1/4">
+        <h3 className=" text-lg font-medium mb-1">Help and Information</h3>
         <ul className="list-unstyled ">
           <li>
             <Link
-              className="hover:text-blue-400"
+              className="text-xs text-gray-500 hover:text-blue-400"
               to="about"
               onClick={scrollToTop}
             >
-              About Us
+              Paper Quality
             </Link>
           </li>
           <li>
             <Link
-              className="hover:text-blue-400"
+              className="text-xs text-gray-500 hover:text-blue-400"
               to="contact"
               onClick={scrollToTop}
             >
-              Contact
+              Privacy Policy
             </Link>
           </li>
           <li>
             <Link
-              className="hover:text-blue-400"
+              className="text-xs text-gray-500 hover:text-blue-400"
               to="track"
               onClick={scrollToTop}
             >
-              Track Order
+              Terms and Conditions
             </Link>
           </li>
           <li>
             <Link
-              className="hover:text-blue-400"
+              className="text-xs text-gray-500 hover:text-blue-400"
               to="blog"
               onClick={scrollToTop}
             >
-              Blog
+              Font Licenses
             </Link>
           </li>
           <li>
             <Link
               to="careers"
-              className=" hover:text-blue-400"
+              className=" text-xs text-gray-500 hover:text-blue-400"
               onClick={scrollToTop}
             >
-              Career
+              Shipping and Delivery
             </Link>
           </li>
         </ul>
       </div>
       {/* third column  */}
-      <div className="sm:w-1/4">
-        <h3 className=" text-lg font-medium mb-4">GET STARTED</h3>
+      <div className="mt-4 sm:w-1/4">
+        <h3 className=" text-lg font-medium mb-1">Get Started</h3>
         <ul className="list-unstyled ">
           <li>
             <Link
               to="affiliates"
-              className=" hover:text-blue-400"
+              className=" text-xs text-gray-500 hover:text-blue-400"
               onClick={scrollToTop}
             >
               Become an Affiliate
@@ -104,7 +102,7 @@ const Footer = () => {
           <li>
             <Link
               to="login"
-              className="font-bold hover:text-blue-400"
+              className="font-bold text-xs text-gray-500 hover:text-blue-400"
               onClick={scrollToTop}
             >
               Login
@@ -113,7 +111,7 @@ const Footer = () => {
           <li>
             <Link
               to="signup"
-              className="font-bold hover:text-blue-400"
+              className="font-bold text-xs text-gray-500 hover:text-blue-400"
               onClick={scrollToTop}
             >
               SignUp
@@ -123,29 +121,8 @@ const Footer = () => {
       </div>
       {/* fourth column  */}
       <div className="sm:w-1/4">
-        <h3 className=" text-lg font-medium mb-4">INFORMATION</h3>
-        <ul className="list-unstyled ">
-          <li>
-            <Link
-              className="hover:text-blue-400"
-              to="privacy_policy"
-              onClick={scrollToTop}
-            >
-              Privacy Policy
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="hover:text-blue-400"
-              to="terms_conditions"
-              onClick={scrollToTop}
-            >
-              Terms & Conditions
-            </Link>
-          </li>
-        </ul>
-
-        <div className="flex mt-6">
+        <h3 className=" text-lg font-medium mb-4 mt-4">Follow Us</h3>
+        <div className="flex -mt-3">
           <a href="#" className="text-blue-400 hover:text-blue-500 mr-2">
             <i className="fab fa-instagram">
               <img src={instagram} alt="" srcset="" className=" h-8" />
@@ -161,6 +138,23 @@ const Footer = () => {
               <img src={linkedin} alt="" srcset="" className=" h-8" />
             </i>
           </a>
+        </div>
+        <div className="sm:w-2/4">
+        <h3 className="text-sm font-medium mb-4 mt-4">Accepted Payments</h3>
+        <div className="flex -mt-3">
+            <i className="fab fa-instagram">
+              <img src={mastercard} alt="" srcset="" className=" h-8" />
+            </i>
+            <i className="fab fa-facebook">
+              <img src={visa} alt="" srcset="" className=" h-8" />
+            </i>
+            <i className="fab fa-linkedin ">
+              <img src={verve} alt="" srcset="" className=" h-8" />
+            </i>
+            <i className="fab fa-linkedin ">
+              <img src={bank} alt="" srcset="" className=" h-8" />
+            </i>
+        </div>
         </div>
       </div>
     </footer>
